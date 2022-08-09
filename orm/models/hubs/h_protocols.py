8c4bcb03c71ae70cf8fd869a30b59f19488c8c16
semyon-puchin <s.puchin@hashcib.com>
2022-08-09 14:23:29 +0300

    Added Hubs

---
+++ orm/models/hubs/h_protocols.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Text, DateTime, Enum
+from sqlalchemy.sql import func
+
+from orm.consts.protocolType import ProtocolType
+from base.main import Base
+
+
+class HubProtocols(Base):
+
+    __tablename__ = 'h_protocols'
+
+    h_protocol_id = Column(Integer, primary_key=True)  # PK
+    h_protocol_name = Column(Text, unique=True, nullable=False)
+    h_protocol_type = Column(Enum(ProtocolType), nullable=False)
+    h_protocol_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
