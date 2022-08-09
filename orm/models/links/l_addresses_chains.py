3b27d0c57174237d22b59a7238e8e67a19286eec
semyon-puchin <s.puchin@hashcib.com>
2022-08-09 14:57:03 +0300

    Added Links

---
+++ orm/models/links/l_addresses_chains.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, ForeignKey
+
+from base.main import Base
+
+
+class LinkAddressesChains(Base):
+
+    __tablename__ = 'l_addresses_chains'
+
+    l_address_chain_id = Column(Integer, primary_key=True)  # PK
+    h_address_id = Column(Integer, ForeignKey('h_addresses.h_address_id'), nullable=False)
+    h_chain_id = Column(Integer, ForeignKey('h_chains.h_chain_id'), nullable=False)
