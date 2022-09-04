df8c290f0f88943aa6041c0721710c91a696790d
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-09-04 21:23:16 +0300

    Added @singleton decorator to Clients

--- orm/clients/writer/client.py
+++ orm/clients/writer/client.py
@@ -1,8 +1,10 @@
 from sqlalchemy.engine.cursor import CursorResult
 
 from head.interfaces.clients.db.client import IClient
+from head.decorators.singleton import singleton
 
 
+@singleton
 class DBWriterClient(IClient):
 
     def execute(self, query: str, *args, **kwargs) -> CursorResult:  # None
