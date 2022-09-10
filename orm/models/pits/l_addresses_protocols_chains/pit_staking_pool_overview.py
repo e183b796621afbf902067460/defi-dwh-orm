31635a1e7a12b209964269bf994a4e81cd896506
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-09-10 18:35:05 +0300

    Added pit_staking_pool_overview model

---
+++ orm/models/pits/l_addresses_protocols_chains/pit_staking_pool_overview.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey, Text
+from sqlalchemy.sql import func
+
+from base.main import Base
+
+
+class PITStakingPoolOverview(Base):
+
+    __tablename__ = 'pit_staking_pool_overview'
+    __table_args__ = {
+        'comment': 'Staking Pool Overview'
+    }
+
+    pit_liquidity_pool_overview_id = Column(Integer, primary_key=True)  # PK
+    l_address_protocol_chain_id = Column(Integer, ForeignKey('l_addresses_protocols_chains.l_address_protocol_chain_id'), nullable=False)
+    pit_token_symbol = Column(Text, nullable=False)
+    pit_token_reserve = Column(Float, nullable=False)
+    pit_token_price = Column(Float, nullable=False)
+    pit_liquidity_pool_overview_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
