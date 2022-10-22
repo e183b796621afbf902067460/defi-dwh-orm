77a4edb044a0f8df3f1ed0c3360fc3b078c8f77f
e183b796621afbf902067460 <109761197+e183b796621afbf902067460@users.noreply.github.com>
2022-10-22 15:47:49 +0300

    Update engine.py

--- cfg/engine.py
+++ cfg/engine.py
@@ -5,7 +5,7 @@ from urllib.parse import quote_plus
 db_address = os.getenv('DB_ADDRESS', '')
 db_user = os.getenv('DB_USER', '')
 db_password = quote_plus(os.getenv('DB_PASSWORD', ''))
-db_name = os.getenv('ORM_DB_NAME', '')
+db_name = os.getenv('DB_NAME', '')
 
 db_url = f'postgresql://{db_user}:{db_password}@{db_address}/{db_name}'
 db_engine = create_engine(db_url)
