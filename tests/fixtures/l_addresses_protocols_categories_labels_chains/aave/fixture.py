c7216d7067e5b3628378bfced27ebda3b26156d8
semyon-puchin <s.puchin@hashcib.com>
2022-10-29 02:45:19 +0500

    Added Aave Position fixture

--- tests/fixtures/l_addresses_protocols_categories_labels_chains/aave/fixture.py
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/aave/fixture.py
@@ -1,40 +1,22 @@
 
 
 FIXTURE = {
-    'USDC/WETH LP': {
+    'Lending Pool': {
         'h_protocols': {
-            'h_protocol_name': 'Uniswap',
-            'h_protocol_category_name': 'DEX'
+            'h_protocol_name': 'Aave',
+            'h_protocol_category_name': 'Lending'
         },
         'h_chains': {
             'h_network_name': 'eth',
             'h_network_id': 1,
             'h_network_endpoint': 'eth.endpoint'
         },
-        'h_addresses': '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
-    },
-    'USDT/WETH LP': {
-        'h_protocols': {
-            'h_protocol_name': 'Uniswap',
-            'h_protocol_category_name': 'DEX'
+        'h_addresses': {
+            'h_pool_address': '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+            'h_wallet_address': '0xcb05e41aE2F23A7356e1Cc0eD7db2cd2dC0f9d22'
         },
-        'h_chains': {
-            'h_network_name': 'eth',
-            'h_network_id': 1,
-            'h_network_endpoint': 'eth.endpoint'
-        },
-        'h_addresses': '0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852'
-    },
-    'USDC/DAI LP': {
-        'h_protocols': {
-            'h_protocol_name': 'Uniswap',
-            'h_protocol_category_name': 'DEX'
-        },
-        'h_chains': {
-            'h_network_name': 'eth',
-            'h_network_id': 1,
-            'h_network_endpoint': 'eth.endpoint'
-        },
-        'h_addresses': '0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5'
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
     },
 }
