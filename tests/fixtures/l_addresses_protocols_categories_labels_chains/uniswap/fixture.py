71e8ec86a41b75361eb83bfcf4122e1719c0573a
semyon-puchin <s.puchin@hashcib.com>
2022-10-29 02:47:27 +0500

    Added Uniswap Position Fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/uniswap/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'USDC/WETH LP': {
+        'h_protocols': {
+            'h_protocol_name': 'Uniswap',
+            'h_protocol_category_name': 'DEX'
+        },
+        'h_chains': {
+            'h_network_name': 'eth',
+            'h_network_id': 1,
+            'h_network_endpoint': 'eth.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc',
+            'h_wallet_address': '0xeC08867a12546ccf53b32efB8C23bb26bE0C04f1'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
