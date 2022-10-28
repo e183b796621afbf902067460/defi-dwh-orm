a040d78358e1e423db0662650cf3dfcf8704575e
semyon-puchin <s.puchin@hashcib.com>
2022-10-29 02:46:36 +0500

    Added Ellipsis Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/ellipsis/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'VALAS/BNB Staking Pool': {
+        'h_protocols': {
+            'h_protocol_name': 'Ellipsis',
+            'h_protocol_category_name': 'Staking'
+        },
+        'h_chains': {
+            'h_network_name': 'bsc',
+            'h_network_id': 2,
+            'h_network_endpoint': 'bsc.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0x6B46dFaC1E46f059cea6C0a2D7642d58e8BE71F8',  # '0x5B74C99AA2356B4eAa7B85dC486843eDff8Dfdbe'
+            'h_wallet_address': '0x818D51ab5cdb5C6872BdF9439707f858854E74CA'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
