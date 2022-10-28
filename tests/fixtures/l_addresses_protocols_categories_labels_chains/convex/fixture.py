3788e326133ec55ada4104202448ced1be497d00
semyon-puchin <s.puchin@hashcib.com>
2022-10-29 02:45:43 +0500

    Added Convex Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/convex/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'FRAX/3CRV Gauge': {
+        'h_protocols': {
+            'h_protocol_name': 'Convex',
+            'h_protocol_category_name': 'Staking'
+        },
+        'h_chains': {
+            'h_network_name': 'eth',
+            'h_network_id': 1,
+            'h_network_endpoint': 'eth.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0xB900EF131301B307dB5eFcbed9DBb50A3e209B2e',
+            'h_wallet_address': '0xcb05e41aE2F23A7356e1Cc0eD7db2cd2dC0f9d22'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
