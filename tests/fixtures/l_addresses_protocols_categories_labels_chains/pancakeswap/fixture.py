e65257a100370f1088478cc220c99d133f4cc7ad
semyon-puchin <s.puchin@hashcib.com>
2022-10-29 02:46:57 +0500

    Added Pancakeswap Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/pancakeswap/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'Autocake': {
+        'h_protocols': {
+            'h_protocol_name': 'Pancakeswap',
+            'h_protocol_category_name': 'Staking'
+        },
+        'h_chains': {
+            'h_network_name': 'bsc',
+            'h_network_id': 2,
+            'h_network_endpoint': 'bsc.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0x45c54210128a065de780C4B0Df3d16664f7f859e',
+            'h_wallet_address': '0xeC08867a12546ccf53b32efB8C23bb26bE0C04f1'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
