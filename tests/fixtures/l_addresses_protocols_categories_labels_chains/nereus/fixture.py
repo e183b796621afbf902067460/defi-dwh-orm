48f775e9dad8ecef7ba9d010d21df33d00f89bd4
semyon-puchin <s.puchin@hashcib.com>
2022-10-30 02:17:25 +0500

    Added Nereus Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/nereus/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'Lending Pool': {
+        'h_protocols': {
+            'h_protocol_name': 'Nereus',
+            'h_protocol_category_name': 'Lending'
+        },
+        'h_chains': {
+            'h_network_name': 'avax',
+            'h_network_id': 5,
+            'h_network_endpoint': 'avax.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0xB9257597EDdfA0eCaff04FF216939FBc31AAC026',
+            'h_wallet_address': '0x000000e28fAA823d5B53ff6C2922c28335840375'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
