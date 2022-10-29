1e475d36c00551b5ddec0698fcc9793b2c5a2909
semyon-puchin <s.puchin@hashcib.com>
2022-10-30 02:18:12 +0500

    Added Sushiswap Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/sushiswap/fixture.py
@@ -0,0 +1,24 @@
+
+
+FIXTURE = {
+    'CVX/ETH LP': {
+        'h_protocols': {
+            'h_protocol_name': 'Sushiswap',
+            'h_protocol_category_name': 'DEX'
+        },
+        'h_chains': {
+            'h_network_name': 'eth',
+            'h_network_id': 1,
+            'h_network_endpoint': 'eth.endpoint'
+        },
+        'h_addresses': {
+            # MasterChef 0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d
+            # pid 1
+            'h_pool_address': '0x05767d9EF41dC40689678fFca0608878fb3dE906',
+            'h_wallet_address': '0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
