f32c1237f585b0c3e7527ceb7983538ddb171010
semyon-puchin <s.puchin@hashcib.com>
2022-10-30 02:17:51 +0500

    Added Sturdy Position fixture

---
+++ tests/fixtures/l_addresses_protocols_categories_labels_chains/sturdy/fixture.py
@@ -0,0 +1,22 @@
+
+
+FIXTURE = {
+    'Lending Pool': {
+        'h_protocols': {
+            'h_protocol_name': 'Sturdy',
+            'h_protocol_category_name': 'Lending'
+        },
+        'h_chains': {
+            'h_network_name': 'eth',
+            'h_network_id': 1,
+            'h_network_endpoint': 'eth.endpoint'
+        },
+        'h_addresses': {
+            'h_pool_address': '0xA422CA380bd70EeF876292839222159E41AAEe17',
+            'h_wallet_address': '0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50'
+        },
+        'h_labels': {
+            'h_label_name': 'DEFI'
+        }
+    },
+}
