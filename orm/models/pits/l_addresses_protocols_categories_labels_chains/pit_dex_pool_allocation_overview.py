339253b714ef7a3e015aa6d79fe97d669f680a77
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-10-31 01:59:27 +0500

    Added PITDEXPoolAllocationOverview model

--- orm/models/pits/l_addresses_protocols_categories_labels_chains/pit_dex_pool_allocation_overview.py
+++ orm/models/pits/l_addresses_protocols_categories_labels_chains/pit_dex_pool_allocation_overview.py
@@ -4,17 +4,16 @@ from sqlalchemy.sql import func
 from base.main import Base
 
 
-class PITLiquidityPoolPositionOverview(Base):
+class PITDEXPoolAllocationOverview(Base):
 
-    __tablename__ = 'pit_liquidity_pool_position_overview'
+    __tablename__ = 'pit_dex_pool_allocation_overview'
     __table_args__ = {
-        'comment': 'Liquidity Pool Position Overview'
+        'comment': 'DEX Pool Allocation Overview'
     }
 
-    pit_liquidity_pool_position_overview_id = Column(Integer, primary_key=True)  # PK
+    pit_dex_pool_allocation_overview_id = Column(Integer, primary_key=True)  # PK
     l_address_protocol_category_label_chain_id = Column(Integer, ForeignKey('l_addresses_protocols_categories_labels_chains.l_address_protocol_category_label_chain_id'), nullable=False)
     pit_token_symbol = Column(Text, nullable=False)
     pit_token_amount = Column(Float, nullable=False)
     pit_token_price = Column(Float, nullable=False)
-    pit_type = Column(Text, nullable=False)
-    pit_liquidity_pool_position_overview_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
+    pit_dex_pool_allocation_overview_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
