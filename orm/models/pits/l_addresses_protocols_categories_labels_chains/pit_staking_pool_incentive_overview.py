959c2631578360b4d45aa323e3bd33bba6fca72a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-01 21:28:24 +0500

    Added StakingPoolIncentiveOverview

---
+++ orm/models/pits/l_addresses_protocols_categories_labels_chains/pit_staking_pool_incentive_overview.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, DateTime, Float, ForeignKey, Text
+from sqlalchemy.sql import func
+
+from base.main import Base
+
+
+class PITStakingPoolIncentiveOverview(Base):
+
+    __tablename__ = 'pit_staking_pool_incentive_overview'
+    __table_args__ = {
+        'comment': 'Staking Pool Incentive Overview'
+    }
+
+    pit_staking_pool_incentive_overview_id = Column(Integer, primary_key=True)  # PK
+    l_address_protocol_category_label_chain_id = Column(Integer, ForeignKey('l_addresses_protocols_categories_labels_chains.l_address_protocol_category_label_chain_id'), nullable=False)
+    pit_token_symbol = Column(Text, nullable=False)
+    pit_token_amount = Column(Float, nullable=False)
+    pit_token_price = Column(Float, nullable=False)
+    pit_staking_pool_incentive_overview_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
